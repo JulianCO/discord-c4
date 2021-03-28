02dccac071f82bde2960f76b8b0700ad6f2e5a34
Julian Osorio <juliancamilo.osorio@gmail.com>
2021-03-27 20:12:30 -0400

    Add retrieve and update capabilities
    CRU down and we are just missing the D

--- src/connect4/persistency.rs
+++ src/connect4/persistency.rs
@@ -1,5 +1,7 @@
 use rusqlite::{Connection};
 use rusqlite::params;
+use std::fmt::format;
+use std::string::String;
 
 use crate::connect4::board::Board;
 
@@ -7,7 +9,10 @@ use crate::connect4::board::Board;
 pub enum NotCompletedReason {
     RedAlreadyPlaying,
     BlueAlreadyPlaying,
-    UnrecoverableError
+    PlayerAlreadyPlaying,
+    PlayerHasNoMatches,
+    UnrecoverableError,
+    InvalidAiLevel,
 }
 
 #[derive(Debug, PartialEq)]
@@ -16,34 +21,68 @@ pub enum Error {
     NotCompleted(NotCompletedReason)
 }
 
+#[derive(Debug, PartialEq, Eq)]
+pub struct HumanMatch { 
+    pub match_id : u64,
+    pub server_id : u64,
+    pub red_player_id : u64,
+    pub blue_player_id : u64,
+    pub board : Board
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct ComputerMatch { 
+    pub match_id : u64,
+    pub server_id : u64,
+    pub player_id : u64,
+    pub player_is_red : bool,
+    pub ai_level : u8,
+    pub board : Board
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum OngoingMatch {
+    HumanMatch(HumanMatch),
+    ComputerMatch(ComputerMatch)
+}
+
+struct DatabaseRow {
+    match_id : i64,
+    server_id : i64,
+    red_player_id : i64,
+    blue_player_id : i64,
+    red_pieces : i64,
+    blue_pieces : i64
+}
+
 impl From<rusqlite::Error> for Error {
     fn from(e : rusqlite::Error) -> Error {
         Error::SqliteError(e)
     }
 }
 
-type Result<T, E = Error> = core::result::Result<T, E>;
+pub type Result<T, E = Error> = core::result::Result<T, E>;
 
 pub fn initialize(db_name : &str) -> Result<Connection> {
-    let mut conn =  Connection::open(db_name)?;
+    let conn =  Connection::open(db_name)?;
     
     conn.execute(
         "CREATE TABLE IF NOT EXISTS matches (
             match_id INTEGER PRIMARY KEY,
             server_id INTEGER NOT NULL,
-            red_player_id INTEGER,
-            blue_player_id INTEGER,
-            red_pieces INTEGER,
-            blue_pieces INTEGER
+            red_player_id INTEGER NOT NULL,
+            blue_player_id INTEGER NOT NULL,
+            red_pieces INTEGER NOT NULL,
+            blue_pieces INTEGER NOT NULL
             );", params![])?;
     Ok(conn)
 }
 
-pub fn new_human_match(conn : &mut Connection, server_id : u64, red_id : u64, blue_id : u64) -> Result<u64> {
+pub fn new_human_match(conn : &mut Connection, server_id : u64, red_id : u64, blue_id : u64) -> Result<HumanMatch> {
     let tx = conn.transaction()?;
     
     let e = Board::empty_board();
-    let (blue_pieces, red_pieces) = e.serialize();
+    let (red_pieces, blue_pieces) = e.serialize();
     
     tx.execute(
         "INSERT INTO matches
@@ -109,11 +148,161 @@ pub fn new_human_match(conn : &mut Connection, server_id : u64, red_id : u64, bl
     
     if match_id == match_id_2 {
         tx.commit()?;
-        Ok(match_id as u64)
+        Ok(
+            HumanMatch {
+                match_id : match_id as u64,
+                server_id,
+                red_player_id : red_id,
+                blue_player_id : blue_id,
+                board : e
+            }
+        )
     } else {
         Err(Error::NotCompleted(NotCompletedReason::UnrecoverableError))
     }
 }
+
+pub fn new_computer_match(conn : &mut Connection, server_id : u64, player_id : u64, player_is_red : bool, ai_level : u8) -> Result<ComputerMatch> {
+    if ai_level > 10 {
+        return Err(Error::NotCompleted(NotCompletedReason::InvalidAiLevel));
+    }
+    
+    let tx = conn.transaction()?;
+    
+    let e = Board::empty_board();
+    let (red_pieces, blue_pieces) = e.serialize();
+    
+    let query = 
+        if player_is_red {
+            format!(
+                "INSERT INTO matches
+                    (server_id, red_player_id, blue_player_id, red_pieces, blue_pieces)
+                    VALUES
+                    (?1, ?2, -{}, ?3, ?4)
+                    ;", ai_level)
+        }
+        else {
+            format!(
+                "INSERT INTO matches
+                    (server_id, red_player_id, blue_player_id, red_pieces, blue_pieces)
+                    VALUES
+                    (?1, -{}, ?2, ?3, ?4)
+                    ;", ai_level)
+        };
+    
+    tx.execute(
+        &query,
+        params![server_id as i64, player_id as i64, red_pieces as i64, blue_pieces as i64])?;
+    
+    let mut stmt1 = tx.prepare(
+        "SELECT match_id 
+            FROM matches 
+            WHERE server_id=?1
+            AND (red_player_id=?2 OR blue_player_id=?2)
+            ;")?;
+    
+    let mut player_found = 
+        stmt1.query_map(
+            params![server_id as i64, player_id as i64], 
+            |row| { row.get(0) }
+            )?;
+    
+    let match_id : i64 = 
+        match player_found.next() {
+            Some(Ok(n)) => 
+                match player_found.next() {
+                    None =>
+                        Ok(n),
+                    Some(_) =>
+                        Err(Error::NotCompleted(NotCompletedReason::PlayerAlreadyPlaying))
+                },
+            Some(Err(r)) =>
+                Err(Error::SqliteError(r)),
+            None =>
+                Err(Error::NotCompleted(NotCompletedReason::UnrecoverableError))
+        }?;
+    
+    drop(player_found);
+    drop(stmt1);
+    
+    tx.commit()?;
+    Ok(
+        ComputerMatch {
+            match_id : match_id as u64,
+            server_id,
+            player_id,
+            player_is_red,
+            ai_level,
+            board : e
+        }
+    )
+}
+
+fn data_row_to_match(row : &DatabaseRow) -> OngoingMatch {
+    let board = Board::unserialize((row.red_pieces as u64, row.blue_pieces as u64));
+    if row.red_player_id < 0 && row.red_player_id > -10 {
+        OngoingMatch::ComputerMatch( ComputerMatch {
+            match_id : row.match_id as u64,
+            server_id : row.server_id as u64,
+            player_id : row.blue_player_id as u64,
+            player_is_red : false,
+            ai_level : (-row.red_player_id) as u8,
+            board
+        })
+    }
+    else if row.blue_player_id < 0 && row.blue_player_id > -10 {
+        OngoingMatch::ComputerMatch( ComputerMatch {
+            match_id : row.match_id as u64,
+            server_id : row.server_id as u64,
+            player_id : row.red_player_id as u64,
+            player_is_red : true,
+            ai_level : (-row.blue_player_id) as u8,
+            board
+        })
+    } 
+    else {
+        OngoingMatch::HumanMatch( HumanMatch {
+            match_id : row.match_id as u64,
+            server_id : row.server_id as u64,
+            red_player_id : row.red_player_id as u64,
+            blue_player_id : row.blue_player_id as u64,
+            board
+        })
+    }
+}
+            
+
+pub fn retrieve_match_by_player(conn : &Connection, server_id : u64, player_id : u64) -> Result<OngoingMatch> {
+    let corresponding_row = conn.query_row(
+        "SELECT match_id, red_player_id, blue_player_id, red_pieces, blue_pieces
+            FROM matches 
+            WHERE server_id=?1
+            AND (red_player_id=?2 OR blue_player_id=?2)
+            ;",
+        params![server_id as i64, player_id as i64],
+        |row| 
+            Ok( DatabaseRow {
+                match_id : row.get(0)?,
+                server_id : server_id as i64,
+                red_player_id : row.get(1)?,
+                blue_player_id : row.get(2)?,
+                red_pieces : row.get(3)?,
+                blue_pieces : row.get(4)?
+            })
+        ).expect("This query right here failed");
+    Ok(data_row_to_match(&corresponding_row))
+}
+
+pub fn update_match_board(conn : &Connection, match_id : u64, board : &Board) -> Result<()> {
+    let (red_pieces, blue_pieces) = board.serialize();
+    conn.execute(
+        "UPDATE matches
+            SET red_pieces = ?1, blue_pieces = ?2
+            WHERE match_id = ?3",
+        params![red_pieces as i64, blue_pieces as i64, match_id as i64])?;
+    Ok(())
+}
+
 
 #[cfg(test)]
 mod test;
--- src/connect4/persistency/test.rs
+++ src/connect4/persistency/test.rs
@@ -1,8 +1,11 @@
 use super::*;
 use std::fs;
+use crate::connect4::board;
+use crate::connect4::board::{Player, GameResult};
 
 #[test]
 fn game_creation_stops_duplicates() {
+    let _ = fs::remove_file("test1.sqlite");
     let mut conn = initialize("test1.sqlite").expect("Failed to create database");
     
     new_human_match(&mut conn, 1, 12, 21).expect("failed to create a game");
@@ -10,17 +13,95 @@ fn game_creation_stops_duplicates() {
     let error_1 = new_human_match(&mut conn, 1, 12, 22).err().expect("Creation should have failed");
     assert_eq!(error_1, Error::NotCompleted(NotCompletedReason::RedAlreadyPlaying));
     
-    let error_2 = new_human_match(&mut conn, 1, 23, 12).err().expect("Creation should have failed");
+    let error_2 = new_human_match(&mut conn, 1, 22, 12).err().expect("Creation should have failed");
     assert_eq!(error_2, Error::NotCompleted(NotCompletedReason::BlueAlreadyPlaying));
     
-    let error_3 = new_human_match(&mut conn, 1, 21, 24).err().expect("Creation should have failed");
+    let error_3 = new_human_match(&mut conn, 1, 21, 22).err().expect("Creation should have failed");
     assert_eq!(error_3, Error::NotCompleted(NotCompletedReason::RedAlreadyPlaying));
     
-    let error_4 = new_human_match(&mut conn, 1, 25, 21).err().expect("Creation should have failed");
+    let error_4 = new_human_match(&mut conn, 1, 22, 21).err().expect("Creation should have failed");
     assert_eq!(error_4, Error::NotCompleted(NotCompletedReason::BlueAlreadyPlaying));
+    
+    let error_5 = new_computer_match(&mut conn, 1, 21, false, 5).err().expect("Creation should have failed");
+    assert_eq!(error_5, Error::NotCompleted(NotCompletedReason::PlayerAlreadyPlaying));
     
     drop(conn);
     fs::remove_file("test1.sqlite").expect("failed to remove temp database");
 }
 
+#[test]
+fn retrieving_empty_game_works() {
+    let _ = fs::remove_file("test2.sqlite");
+    let mut conn = initialize("test2.sqlite").expect("Failed to create database");
+    
+    let e = Board::empty_board(); 
+    
+    new_human_match(&mut conn, 1, 12, 21).expect("failed to create a game");
+    new_computer_match(&mut conn, 1, 13, true, 5).expect("failed to create a game");
+    
+    let found_human_match = retrieve_match_by_player(&conn, 1, 21).expect("Match just created not found");
+    let found_computer_match = retrieve_match_by_player(&conn, 1, 13).expect("Match just created not found");
+    
+    match found_human_match {
+        OngoingMatch::ComputerMatch(_) => 
+            panic!("Found computer match where a human match was inserted"),
+        OngoingMatch::HumanMatch(m) => 
+            assert_eq!(m.board.serialize(), e.serialize())
+    }
+    
+    match found_computer_match {
+        OngoingMatch::HumanMatch(_) => 
+            panic!("Found human match where a computer match was inserted"),
+        OngoingMatch::ComputerMatch(m) => 
+            assert_eq!(m.board.serialize(), e.serialize())
+    }
+    
+    drop(conn);
+    fs::remove_file("test2.sqlite").expect("failed to remove temp database");
+}
 
+fn example_match_in_database(conn: &mut Connection, p1 : u64, p2 : u64,moves : &[u8], expected_result : board::GameResult) {
+    let mut e = Board::empty_board();
+    let mut m : OngoingMatch;
+    let mut m_id = 444444u64;
+    
+    new_human_match(conn, 1, p1, p2).expect("Failed to create a game");
+    
+    for k in moves {
+        m = retrieve_match_by_player(conn, 1, p2).expect("Failed to retrieve game");
+        e = match m {
+            OngoingMatch::ComputerMatch(_) =>
+                panic!("Found computer match where a human match was inserted"),
+            OngoingMatch::HumanMatch(h) => {
+                m_id = h.match_id;
+                h.board
+            }
+        };
+        assert!(e.is_move_legal(*k));
+        e.play_move(*k);
+        
+        update_match_board(conn, m_id, &e);
+    }
+    assert!(e.game_status() == board::GameStatus::GameOver(expected_result));
+}
+
+#[test]
+fn test_sequencial_games_in_database() {
+    let _ = fs::remove_file("test3.sqlite");
+    let mut conn = initialize("test3.sqlite").expect("Failed to create database");
+    
+    let game1 = vec![3, 3, 2, 1, 3, 5, 2, 2, 4, 3, 1, 2, 5, 1, 1, 3, 5, 5, 5, 2, 1, 2];
+    let game2 = vec![3, 3, 3, 5, 5, 2, 3, 3, 2, 2, 2, 2, 6, 1, 4, 6, 6, 6, 1, 3, 1, 0, 1, 1, 4, 6, 4];
+    let game3 = vec![2, 3, 3, 3, 2, 2, 5, 5, 3, 5, 5, 2, 2, 6, 6, 3, 3, 5, 4, 0, 0, 0, 5, 6, 1, 1, 1, 6, 0, 4];
+    let game4 = vec![4, 3, 3, 3, 3, 4, 5, 4, 4, 0, 3, 0, 3, 4, 2, 4, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 2, 2, 2, 5, 5, 5, 6, 6, 6, 5, 6, 5, 2, 2, 6, 6];
+    
+    
+    example_match_in_database(&mut conn, 12, 21, &game1, GameResult::Winner(Player::Blue));
+    example_match_in_database(&mut conn, 13, 31, &game2, GameResult::Winner(Player::Red));
+    example_match_in_database(&mut conn, 14, 41, &game3, GameResult::Winner(Player::Blue));
+    example_match_in_database(&mut conn, 15, 51, &game4, GameResult::Tie);
+    
+    drop(conn);
+    fs::remove_file("test3.sqlite").expect("failed to remove temp database");
+}
+    
